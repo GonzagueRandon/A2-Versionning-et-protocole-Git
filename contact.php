e5db70c74a730c2cae9dda8c4fadf4dfa06619d4
ra gonzague <jeuok@yahoo.fr>
2016-09-16 00:57:22 +0200

    add css on thee concat page

--- contact.php
+++ contact.php
@@ -5,14 +5,14 @@
   <meta charset="UTF-8">
   <title>Contact</title>
   <meta name="description" content="Site Web de SounCloud Streaming Music "
-  <meta name="keywords" content="Web, Attraction, Space Mountain 2, sensation forte, DISNEYLAND, Paris, HTML,CSS,JavaScript, fête foraine,Parc d'attraction, réferencement ">
+  
   <meta name="author" content="Adam Benhaddou">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="../view/css/contact.css">
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="/view/css/contact.css">
   
 </head>
 <body>
