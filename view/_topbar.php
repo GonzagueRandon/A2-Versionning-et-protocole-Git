3053254335f9ac0a52e8ee1d0a4001eee48a3dea
ra gonzague <jeuok@yahoo.fr>
2016-09-16 13:41:06 +0200

    correction of code on -topbar page

--- view/_topbar.php
+++ view/_topbar.php
@@ -31,10 +31,6 @@
                         ?>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
 						<a href="addclip.php" class="btn"><i class="fa fa-plus"></i> Ajouter un clip</a>
-
-                        <a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter un clip</a>
-
-						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
 						<a href="dashboard.php" class="btn"><i class="fa fa-plus"></i> Playlist</a>
 						<a href="dashboard.php" class="btn"> 
 						Accueil</a>
