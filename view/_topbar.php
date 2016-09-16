a8131a9385f82f2b07cf4ab25b2d372c80879177
adam <adambenhaddou@gmail.com>
2016-09-16 10:25:53 +0200

    Add php session id member to the topbar.php

--- view/_topbar.php
+++ view/_topbar.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <div class="row" style="background-color: #333;margin:0">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		<div id="topbar" class="clearfix">
@@ -7,13 +8,33 @@
 						<a href="index.php" class="logo"><i class="fa fa-soundcloud"></i></a>
 					</div>
 					<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
-						<a href="logout.php" class="btn">Déconnexion</a>
-						<a href="contact.php" class="btn"> contact</a>
-						<a href="edit_image.php" class="btn">Profil</a>
+
+						<a href="contact.php" class="btn">Contact</a>
+
+						
+						
+
+                        <?php    if (isset($_SESSION["id_member"])) {
+                        ?>
+                        <a href="login.php" class="btn">Se connecter</a>
+                        <a href="register.php" class="btn">S'inscrire</a>
+                        <?php
+                        }
+                        else {                
+                        ?>   
+                        
+                        <a href="logout.php" class="btn">Se déconnecter</a> 
+
+                        <?php
+                        }
+                        ?>
+
+
+
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter un clip</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
-						<a href="add_music.php" class="btn"> Ma playlist</a>
+						<a href="dashboard.php" class="btn"> Ma playlist</a>
 					</div>
 				</div>
 			</div>
