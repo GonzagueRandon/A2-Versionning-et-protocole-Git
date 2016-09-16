c4d923e28fb2487efbbbc1f766ecf58f84bbff03
Unknown <jeuok@yahoo.fr>
2016-09-16 15:11:52 +0200

    Modification on contact page

--- contact.php
+++ contact.php
@@ -48,7 +48,7 @@
                         <h2>Contact</h2>
                     </div>
                 </div>
-                w </div>
+                 </div>
 
         </div>
         <!-- Formulaire de Contact -->
@@ -58,7 +58,7 @@
                     <div class="col-sm-12 col-lg-12">
                         <h1 class="h1">
                     Contact us <small>We are listening to you</small></h1>
-                        <img src="images/soundcloud0.jpg" style="width:304px;height:228px;">
+                        <img src="images/soundcloud0.jpg" style="width:100px;height:100px;">
                     </div>
                 </div>
             </div>
@@ -71,6 +71,7 @@
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group">
+                                        <font color="black">
                                         <label for="name">
                                             Name</label>
                                         <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
@@ -102,6 +103,7 @@
                                         <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required" placeholder="Message"></textarea>
                                     </div>
                                 </div>
+                                </font>
                                 <div class="col-md-12">
                                     <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
                                         Send Message</button>
