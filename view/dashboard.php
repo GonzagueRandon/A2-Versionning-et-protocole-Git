b1e9dce7e9c16534e2c7b5beb7fb94bbf5f26001
adam <adambenhaddou@gmail.com>
2016-09-16 16:09:44 +0200

    Change background dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,20 +1,9 @@
 <body>
 	<?php include '_topbar.php'; ?>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div id="musicfeed">
-					
-					
-								
-								
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="container1">
+		<img src="images/bg.jpg" class="bg" alt="bg" />
+		
 	</div>
 </body>
 
